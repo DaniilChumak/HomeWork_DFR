c220f611cc2602b469cba95563e8fcfbaf8368b4
dchumak <dchvgithub@gmail.com>
2024-10-02 23:05:27 +0500

    Прописан сериалайзер вывода количества уроков в курсе но почему не появляется

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,11 +1,22 @@
 from materials.models import Course, Lesson
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 
 class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    count_lesson_in_course = SerializerMethodField()
+
+    def get_count_lesson_in_course(self, course):
+        return Course.object.filter(Lesson=course.lesson).count()
+
+    class Meta:
+        model = Course
+        fields = ("title", "preview_image", "description", "count_lesson_in_course")
 
 
 class LessonSerializer(ModelSerializer):
