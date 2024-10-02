900d2535abd798a4e253a4c18722ff4d6697b4d8
dchumak <dchvgithub@gmail.com>
2024-10-02 23:06:08 +0500

    Прописан сериалайзер для вывода поля количества уроков в курсе, но не работает

--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from materials.models import Course, Lesson
-from materials.serializers import CourseSerializer, LessonSerializer
+from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
 from rest_framework.generics import (
     CreateAPIView,
     RetrieveAPIView,
@@ -12,7 +12,11 @@ from rest_framework.generics import (
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CourseDetailSerializer
+        return CourseSerializer
 
 
 class LessonCreateAPIView(CreateAPIView):
