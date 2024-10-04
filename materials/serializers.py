75b9c652bda0dcae3ec82d54e8fdf11f4447252e
dchumak <dchvgithub@gmail.com>
2024-10-04 21:37:00 +0500

    Получилось вывести информацию по урокам в курсе

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,24 +2,30 @@ from materials.models import Course, Lesson
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
-class CourseDetailSerializer(ModelSerializer):
-    count_lesson_in_course = SerializerMethodField()
-
-    def get_count_lesson_in_course(self, course):
-        return Course.object.filter(Lesson=course.lesson).count()
-
-    class Meta:
-        model = Course
-        fields = ("title", "preview_image", "description", "count_lesson_in_course")
-
-
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseDetailSerializer(ModelSerializer):
+    lesson_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
+    subscription = SerializerMethodField()
+
+
+    def get_lesson_count(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    class Meta:
+        model = Course
+        fields = ('title', 'description', 'lesson_count', 'lessons')
+
