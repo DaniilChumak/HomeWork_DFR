bf12c3c17bcd7fad2c54afd7bdf46a20992bbf4f
dchumak <dchvgithub@gmail.com>
2024-10-05 19:19:25 +0500

    Реализовали CRUD для пользователей, в том числе регистрацию пользователей

--- users/views.py
+++ users/views.py
@@ -1,14 +1,57 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
-from rest_framework.viewsets import ModelViewSet
-from users.models import Payment
-from users.serializers import PaymentSerializer
+
+from rest_framework import generics, viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
+from rest_framework.generics import ListAPIView
 
 
-class PaymentViewSet(ModelViewSet):
-    queryset = Payment.objects.all()
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """Опция создания нового юзера"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    """Настройка для разрешения создавать нового юзера всем"""
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserListAPIView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-    ordering_fields = ('date_of_payment',)
-    search_fields = ('payment_method',)
-    filterset_fields = ('payment_date', 'course_paid', 'lesson_paid', 'payment_method',)
+    queryset = Payment.objects.all()
+    """Настроили фильтрацию"""
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['paid_lesson', 'paid_course', 'payment_method']
+    ordering_fields = ['-date_of_payment']
