465ff9dab23feb8abd7328d242966119d255c5aa
dchumak <dchvgithub@gmail.com>
2024-10-06 20:54:50 +0500

    Добавили валидатор в сериализатор для проверки ссылки добавляемой в материалы

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,28 +1,29 @@
 from materials.models import Course, Lesson
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from materials.validators import LinkToVideoValidator
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [LinkToVideoValidator(field="link_to_video")]
 
 
 class CourseSerializer(ModelSerializer):
     lesson_count = SerializerMethodField()
-    lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
+    lessons = LessonSerializer(many=True, read_only=True, source="lesson_set")
 
     def get_lesson_count(self, obj):
         return obj.lesson_set.count()
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CourseDetailSerializer(ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ('title', 'description', 'lesson_count', 'lessons')
-
+        fields = ("title", "description", "lesson_count", "lessons")
