2bd854512d4cb25a3759b4984289ed31961bb6ae
dchumak <dchvgithub@gmail.com>
2024-10-06 20:58:44 +0500

    Добавили модель подписки на курс

--- materials/models.py
+++ materials/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from config.settings import AUTH_USER_MODEL
 
 
 class Course(models.Model):
@@ -60,3 +61,31 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        blank=True,
+        null=True,
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name='Курс',
+        blank=True,
+        null=True,
+    )
+    sign_of_subscription = models.BooleanField(
+        default=False,
+        verbose_name='Признак подписки'
+    )
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+
+    def __str__(self):
+        return f'{self.user}: {self.course}'
