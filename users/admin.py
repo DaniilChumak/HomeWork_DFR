17428b7d3c2a1c233a6062b5dd50c6d43a60b1eb
dchumak <dchvgithub@gmail.com>
2024-10-09 20:27:43 +0500

    Добавили админку

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Payment, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'date_of_payment', 'course_paid', 'lesson_paid', 'cost', 'payment_method')
+    list_filter = ('user', 'date_of_payment', 'course_paid', 'lesson_paid', 'cost', 'payment_method')
+    search_fields = ('payment_method',)
